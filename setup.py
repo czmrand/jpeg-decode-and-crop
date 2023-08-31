e1a83ec0eda2f2e947ec84cd85d84c0023f1d825
Yitzhak Levi <yitzhak.levi@mobileye.com>
2023-08-31 10:56:54 +0300

    remove files

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from torch.utils import cpp_extension
-
-setup(name='decode_and_crop_jpeg',
-      ext_modules=[cpp_extension.CppExtension('decode_and_crop_jpeg', 
-                                              ['decode_and_crop_jpeg.cpp'], 
-                                              libraries=['jpeg'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
